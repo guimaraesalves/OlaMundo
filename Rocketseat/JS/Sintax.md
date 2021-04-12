283e8df5a46ae37477576b276c8944f03e93a943
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-04-12 17:38:02 -0300

    mudança de local da pasta Rocketseat

--- Rocketseat/JS/Sintax.md
+++
@@ -1,9 +0,0 @@
-# A importância da Sintaxe
-
-* Toda linguagem tem
-* Uma boa comunicação necessita de uma boa sintaxe
-* 82% dos erros para iniciantes programação
-
-```js
-    console.log("Bem vindos ao Starter")
-```
--- Rocketseat/JS/comentarios.js
+++
@@ -1,9 +0,0 @@
-// Aqui eu tenho um comentário em linha
-
-// o código abaixo escreve alguma mensagem no devtools
-
-console.log('OLá, Mundo!! Pela miléssima vez.') // alguma coisa
-
-/*
-Aqui eu tenho um comentário de bloco
-*/
